206cada4789271c84c78339c37691c6fcce96d77
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-10-06 23:03:30 +0300

    Dev:-Added Focus on Input Field

--- src/App.js
+++ src/App.js
@@ -162,14 +162,17 @@ function Search({ query, setQuery }) {
   const inputEl = useRef(null);
   useEffect(() => {
     const callBack = (e) => {
+      if(document.activeElement===inputEl.current)
+      return;
       if (e.code === "Enter") {
         inputEl.current.focus();
+        setQuery("")
       }
     };
     document.addEventListener("keydown", callBack);
 
     return () => document.addEventListener("keydown", callBack);
-  }, []);
+  }, [setQuery]);
   return (
     <>
       <input
